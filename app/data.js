935ea00f5a32145a49304b39bd92bbc3b198cada
Johannes Skjærbæk <johannes.skjaerbaek@gmail.com>
2018-03-25 20:41:12 +0200

    Setting CORS.

--- app/data.js
+++ app/data.js
@@ -144,6 +144,12 @@ const getAllData = async () => {
   }
 };
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/', async (req, res) => {
   const data = await getAllData();
   res.send(data);
